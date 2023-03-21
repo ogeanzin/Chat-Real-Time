dde3d754560e58c775b4e758a7e181d46efa110c
Gean Victor <geanvictorduque@gmail.com>
2023-03-21 10:17:34 -0300

    Finish login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,20 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import io from 'socket.io-client'
+import { useState } from 'react'
 
 import Home from './pages/Home';
 
 import './App.css';
 
+const socket = io('http://localhost:3536')
+
 function App() {
+  const [username, setUsername] = useState('')
+  const [room, setRoom] = useState('')
   return (
       <BrowserRouter>
         <Routes>
-            <Route path='/' element={<Home />} />
+            <Route path='/' element={<Home username={username} setUsername={setUsername} room={room} setRoom={setRoom} socket={socket} />} />
         </Routes>
     </BrowserRouter>
   );
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,17 +1,36 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 import './Home.css'
 
-export const Home = () => {
-  return (
-    <div className="home-container">
-        <div className="login-wrapper">
-            <h1>Login Chat</h1>
-            <input type="text" placeholder='Username' />
-            <button>LOGIN</button>
+export const Home = ({ username, setUsername, room, setRoom, socket }) => {
+    const navigate = useNavigate()
+
+    function joinRoom() {
+        if (username !== '' && room !== '') {
+            socket.emit('join_room', { username, room })
+        }
+
+        navigate('/chat', { replace: true })
+    }
+
+    return (
+        <div className="home-container">
+            <div className="login-wrapper">
+                <h1>Login Chat</h1>
+
+                <input className='input' type="text" placeholder='Username' required onChange={(e) => setUsername(e.target.value)}/>
+
+                <select className='input' onChange={(e) => setRoom(e.target.value)}>
+                    <option>Select Room</option>
+                    <option value="sports">Sports</option>
+                    <option value="games">Games</option>
+                </select>
+
+                <button type='submit' onClick={joinRoom}>LOGIN</button>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Home
