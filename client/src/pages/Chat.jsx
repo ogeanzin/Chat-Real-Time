f162e9f4caef8165cb1936f786a865007a13857a
Gean Victor <geanvictorduque@gmail.com>
2023-04-27 12:32:08 -0300

    Add/config Tailwindcss and React Query

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from 'react';
 
 const Chat = () => {
    return (
-    <div>       
+    <div>
+     
     </div>
   )
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
-import './Home.css'
-
 export const Home = () => {
     const navigate = useNavigate()
     const [username, setUsername] = useState('')
