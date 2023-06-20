f771836c31efaabc6924e1fdd7284f74f0bfec52
Gean Victor <geanvictorduque@gmail.com>
2023-06-20 16:40:42 -0300

    Create ChatBar / ChatHeader

---
+++ client/src/Components/Chat/ChatHeader.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CloseIcon from '@mui/icons-material/Close';
+
+const ChatHeader = () => {
+  return (
+    <div>
+        <CloseIcon className='text-red-800 cursor-pointer' />
+    </div>
+  )
+}
+
+export default ChatHeader
--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -3,14 +3,18 @@ import React, { useEffect, useState } from 'react';
 // Components
 import ChatFooter from '../Components/Chat/ChatFooter';
 import ChatBody from '../Components/Chat/ChatBody';
+import ChatBar from '../Components/Chat/ChatBar';
+import ChatHeader from '../Components/Chat/ChatHeader';
 
 const Chat = (socket) => {
 
    return (
     <div className='flex justify-center items-center p-2 h-screen'>
        <div className='w-1/2 flex flex-col items-center justify-center'>
-         <ChatBody />
-         <ChatFooter socket={socket} />
+          <ChatHeader />
+          <ChatBar />
+          <ChatBody />
+          <ChatFooter socket={socket} />
        </div>
        
     </div>
